18104734431e8bdefb311fb8c1a455895a79d32f
ELnurAbdimanapk <elnurabdimanapkyzy@gmail.com>
2023-07-12 13:36:18 +0600

    added answer part

--- script.js
+++ script.js
@@ -1,40 +1,70 @@
-let operand1 = Math.floor(Math.random()*10)
-let operand2 = Math.floor(Math.random()*10)
-const  operators = ["+" ,"-", "/", " *"]
-const index = Math.floor(Math.random() * operators.length)
-const operator = operators[index]
 
-const calculateOperators = (operand1,operand2,operator) => {
- if(operator == "+") {
-    return operand1 + operand2
- }
- else if(operator === "-") {
-    return operand1 + operand2
- }
- else if(operator === "*") {
-    return operand1 * operand2
- }
- else if(operator === "/") {
-    return operand1 / operand2
- }else{
-    return "JOK tuura emes"
- }
+const operand1Element = document.getElementById('first__operand');
+const operand2Element = document.getElementById('second__operand');
+const operatorElement = document.getElementById('operator');
+const inputElement = document.getElementById('answer')
+const winElement = document.getElementById('win');
+
+const calculateOperators = (operand1, operand2, operator) => {
+  if (operator === "+") {
+    return operand1 + operand2;
+  } else if (operator === "-") {
+    return operand1 - operand2;
+  } else if (operator === "*") {
+    return operand1 * operand2;
+  } else if (operator === "/") {
+    return operand1 / operand2;
+  } else {
+    return "JOK tuura emes";
+  }
 }
 
-const answer = calculateOperators(operand1,operand2,operator)
-document.getElementById('first__operand').innerText =operand1
-document.getElementById('second__operand').innerText= operand2
-document.getElementById('operator').innerText=operator
-let inputElement = document.getElementById('answer');
-inputElement.addEventListener('input',function () {
-    let inputValue = inputElement.value
-})
+const generateExample = () => {
+  const operand1 = Math.floor(Math.random() * 10);
+  const operand2 = Math.floor(Math.random() * 10);
+  const operators = ["+", "-", "/", "*"];
+  const operator = operators[Math.floor(Math.random() * operators.length)];
+  const answer = calculateOperators(operand1, operand2, operator);
+  
 
-document.getElementById('submit').addEventListener('click',function() {
-    console.log(inputValue);
-   if(answer === inputValue) {
-    console.log("tuuram");
-   }else{
-    console.log("tuurames");
+  return { operand1, operand2, operator, answer };
+}
+
+const renderExample = (data) => {
+  operand1Element.innerText = data.operand1;
+  operand2Element.innerText = data.operand2;
+  operatorElement.innerText = data.operator;
+}
+
+// Win
+const checkBtn = document.querySelector('#submit');
+let win = 0;
+let example = generateExample();
+renderExample(example);
+
+checkBtn.addEventListener('click', () => {
+  const inputValue = inputElement.value;
+  if (!inputValue && inputValue !== "0") return;
+  win += inputValue == example.answer ? 1 : -1;
+  winElement.textContent = win;
+  inputElement.value = "";
+  example = generateExample();
+  renderExample(example);
+  inputElement.focus()
+});
+
+
+window.addEventListener('keydown', function(event) {
+   if (event.key === 'Enter' && document.activeElement === inputElement) {
+     const inputValue = inputElement.value;
+     if (!inputValue && inputValue !== "0") return;
+     win += inputValue == example.answer ? 1 : -1;
+     winElement.textContent = win;
+     inputElement.value = "";
+     example = generateExample();
+     renderExample(example);
+     inputElement.focus()
+
    }
-})
+ });
+ 
