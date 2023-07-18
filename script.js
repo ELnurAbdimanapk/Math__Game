9f3443c5ab977f8a096f5366e83d6d4ecf23a0ec
ELnurAbdimanapk <elnurabdimanapkyzy@gmail.com>
2023-07-18 13:02:14 +0600

    added action part

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const operand2Element = document.getElementById('second__operand');
 const operatorElement = document.getElementById('operator');
 const inputElement = document.getElementById('answer')
 const winElement = document.getElementById('win');
+const action = document.getElementById('action')
 
 const calculateOperators = (operand1, operand2, operator) => {
   if (operator === "+") {
@@ -19,14 +20,15 @@ const calculateOperators = (operand1, operand2, operator) => {
   }
 }
 
+const updateProblems = () => {
+  state.currentProblem = generateExample()
+}
 const generateExample = () => {
   const operand1 = Math.floor(Math.random() * 10);
   const operand2 = Math.floor(Math.random() * 10);
   const operators = ["+", "-", "/", "*"];
   const operator = operators[Math.floor(Math.random() * operators.length)];
   const answer = calculateOperators(operand1, operand2, operator);
-  
-
   return { operand1, operand2, operator, answer };
 }
 
@@ -40,31 +42,38 @@ const renderExample = (data) => {
 const checkBtn = document.querySelector('#submit');
 let win = 0;
 let example = generateExample();
+console.log(example);
 renderExample(example);
 
-checkBtn.addEventListener('click', () => {
+function checkAnswer() {
   const inputValue = inputElement.value;
   if (!inputValue && inputValue !== "0") return;
-  win += inputValue == example.answer ? 1 : -1;
+  if (inputValue == example.answer) {
+    win += 1;
+    action.innerHTML = "+1";
+  } else {
+    win -= 1;
+    action.innerHTML = "-1";
+  }
+  
   winElement.textContent = win;
   inputElement.value = "";
   example = generateExample();
   renderExample(example);
-  inputElement.focus()
-});
+  inputElement.focus();
 
+  action.style.display = "block";
+  
+  setTimeout(() => {
+    action.style.display = "none";
+  }, 1000);
+}
+
+
+checkBtn.addEventListener('click', checkAnswer);
 
 window.addEventListener('keydown', function(event) {
-   if (event.key === 'Enter' && document.activeElement === inputElement) {
-     const inputValue = inputElement.value;
-     if (!inputValue && inputValue !== "0") return;
-     win += inputValue == example.answer ? 1 : -1;
-     winElement.textContent = win;
-     inputElement.value = "";
-     example = generateExample();
-     renderExample(example);
-     inputElement.focus()
-
-   }
- });
- 
+  if (event.key === 'Enter' && document.activeElement === inputElement) {
+    checkAnswer();
+  }
+});
